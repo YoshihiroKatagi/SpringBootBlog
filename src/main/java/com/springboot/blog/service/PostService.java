173e107adc70549c56ef98c45ca01c67a9f8fef3
katagi <katagiyoshihiro@gmail.com>
2023-05-24 00:47:34 +0900

    updated

--- src/main/java/com/springboot/blog/service/PostService.java
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -1,26 +1,3 @@
-//package com.springboot.blog.service;
-//
-//import com.springboot.blog.payload.PostDto;
-//import com.springboot.blog.payload.PostResponse;
-//
-//import java.util.List;
-//
-//public interface PostService {
-//    PostDto createPost(PostDto postDto);
-//
-//    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
-//
-//    PostDto getPostById(long id);
-//
-//    PostDto updatePost(PostDto postDto, long id);
-//
-//    void deletePostById(long id);
-//}
-
-
-
-
-
 package com.springboot.blog.service;
 
 import com.springboot.blog.payload.PostDto;
@@ -38,6 +15,4 @@ public interface PostService {
     PostDto updatePost(PostDto postDto, long id);
 
     void deletePostById(long id);
-
-//    List<PostDto> getPostsByCategory(Long categoryId);
 }
