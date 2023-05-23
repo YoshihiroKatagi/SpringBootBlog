3dd71cc40332df7109561d8443e0c6a6444eeefe
katagi <katagiyoshihiro@gmail.com>
2023-05-24 00:26:19 +0900

    updated

--- src/main/java/com/springboot/blog/utils/PasswordGeneratorEncoder.java
+++ src/main/java/com/springboot/blog/utils/PasswordGeneratorEncoder.java
@@ -1,20 +1,3 @@
-//package com.springboot.blog.utils;
-//
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//public class PasswordGeneratorEncoder {
-//    public static void main(String[] args) {
-//        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        System.out.println(passwordEncoder.encode("admin"));
-//        System.out.println(passwordEncoder.encode("ramesh"));
-//    }
-//}
-
-
-
-
-
 package com.springboot.blog.utils;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,3 +10,20 @@ public class PasswordGeneratorEncoder {
         System.out.println(passwordEncoder.encode("ramesh"));
     }
 }
+
+
+
+
+
+//package com.springboot.blog.utils;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//public class PasswordGeneratorEncoder {
+//    public static void main(String[] args) {
+//        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        System.out.println(passwordEncoder.encode("admin"));
+//        System.out.println(passwordEncoder.encode("ramesh"));
+//    }
+//}
