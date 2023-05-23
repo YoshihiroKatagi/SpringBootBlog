33808d814aeb3de58181463c455804b95b53b85c
katagi <katagiyoshihiro@gmail.com>
2023-05-23 20:13:34 +0900

    first commit

---
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CategoryDto;
+import com.springboot.blog.service.CategoryService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // Build Add Category REST API
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
+        CategoryDto savedCategory = categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    // Build Get Category REST API
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable("id") Long categoryId) {
+        CategoryDto categoryDto = categoryService.getCategory(categoryId);
+        return ResponseEntity.ok(categoryDto);
+    }
+}
